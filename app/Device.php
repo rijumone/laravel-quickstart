bf183dec7b19644bab3880c29c422cebf144b246
rijumone <mailmeonriju@gmail.com>
2016-06-16 22:38:48 +0530

    again

---
+++ app/Device.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Device extends Model
+{
+    protected $fillable = [
+    	'brand',
+    	'model',
+    	'variant',
+
+    ];
+}
